e403d27ed4ebc97aee07a5bba6b31eae2f281293
dcolmer <dcolmer@statestreet.com>
2019-05-21 15:31:44 +0100

    adding render template and new templates folder

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # create_app wraps the other functions to set up the project
 
@@ -8,8 +8,10 @@ def create_app(config=None, testing=False, cli=True):
     """
     app = Flask(__name__, static_folder=None)
 
-    @app.route("/")
-    def hello():
-        return "Hello World!"
+    @app.route('/')
+    @app.route('/index')
+    def index():
+        user = {'username':'David'}
+        return render_template('index.html', title='Home Page', user=user)
 
     return app
